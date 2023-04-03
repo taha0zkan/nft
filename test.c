c0a536d841f48568c40fd1f705fd6e55700b9e3e
taha0zkan <taha.34.ozkan@gmail.com>
2023-04-03 23:57:24 +0300

    first commit

---
+++ test.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World");
+    printf("Hello World");
+   
+    printf("Hello World");
+   
+    printf("Hello World");
+   
+    printf("Hello World");
+   
+    printf("Hello World");
+   
+    printf("Hello World");
+    printf("naber");
+    printf("naber");
+    printf("naber");
+   
+   
+}
